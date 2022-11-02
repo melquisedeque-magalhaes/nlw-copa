af0833910f631e3cdbf7824b03e554aab2164d0a
melquisedeque_sodre <43079783+melquisedeque-magalhaes@users.noreply.github.com>
2022-11-02 16:40:40 -0300

    create routes couter users and guesses and create router create pool

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import { PrismaClient } from '@prisma/client'
 import cors  from '@fastify/cors'
+import { z } from 'zod'
+import ShortUniqueId from 'short-unique-id'
 
 const prisma = new PrismaClient({
   log: ['query']
@@ -21,6 +23,57 @@ async function bootstrap() {
      return {
       count
     }
+  })
+
+  fastify.get('/users/count', async () => {
+    const count = await prisma.user.count()
+
+     return {
+      count
+    }
+  })
+
+  fastify.get('/guesses/count', async () => {
+    const count = await prisma.guess.count()
+
+     return {
+      count
+    }
+  })
+
+  fastify.post('/pool', async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string({
+        required_error: "type string is required",
+      }).min(3, {
+        message: 'min 3 character'
+      })
+    })
+
+    try {
+      const { title } = createPoolBody.parse(request.body)
+
+      const generate = new ShortUniqueId({ length: 6 })
+      const code = String(generate()).toUpperCase()
+
+      await prisma.pool.create({
+        data: {
+          title,
+          code,
+        }
+      })
+
+      return reply.status(201).send({ code })
+    } catch(error) {
+        if (error instanceof z.ZodError) {
+          return reply.status(400).send({
+            statusCode: 400,
+            error: error.issues[0].code,
+            message: error.issues[0].message
+          })
+        }
+    }
+    
   })
 
   await fastify.listen({
