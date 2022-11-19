49008b8d43e5b5281f358ef559166dc2fe1d10b7
melquisedeque_sodre <43079783+melquisedeque-magalhaes@users.noreply.github.com>
2022-11-19 15:26:31 -0300

    add analictys

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { signIn, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import { setCookie } from 'cookies-next'
 import toast, { Toaster } from 'react-hot-toast'
+import { Analytics } from '@vercel/analytics/react'
 
 import AppNlwMobileImg from '../assets/app-trilha-ignite.png'
 import Logo from '../assets/logo.svg'
@@ -40,29 +41,32 @@ export default function Login() {
   }, [status])
 
   return (
-    <div className="max-w-[1440px] h-screen mx-auto grid grid-cols-2 flex items-center justify-center gap-28">
-      <main className="max-w-[489px]">
-        <Image src={Logo} alt="Logo do NLW" />
+    <>
+      <div className="max-w-[1440px] h-screen mx-auto grid grid-cols-2 flex items-center justify-center gap-28">
+        <main className="max-w-[489px]">
+          <Image src={Logo} alt="Logo do NLW" />
 
-        <h1 className="mt-[60px] mb-10 text-5xl font-bold font-sans text-white">
-          Faça login e crie seu bolão!
-        </h1>
+          <h1 className="mt-[60px] mb-10 text-5xl font-bold font-sans text-white">
+            Faça login e crie seu bolão!
+          </h1>
 
-        <button
-          onClick={() => signIn('google')}
-          className="h-14 w-full bg-red-500 rounded font-sans font-bold text-white mt-16 flex items-center justify-center gap-2 hover:bg-red-600 transition-colors uppercase"
-        >
-          <GoogleLogo weight="bold" color="#fff" size={24} />
-          ENTRAR COM GOOGLE
-        </button>
-      </main>
+          <button
+            onClick={() => signIn('google')}
+            className="h-14 w-full bg-red-500 rounded font-sans font-bold text-white mt-16 flex items-center justify-center gap-2 hover:bg-red-600 transition-colors uppercase"
+          >
+            <GoogleLogo weight="bold" color="#fff" size={24} />
+            ENTRAR COM GOOGLE
+          </button>
+        </main>
 
-      <Image
-        quality={100}
-        alt="dois celulares com o aplicativo do NLW aberto"
-        src={AppNlwMobileImg}
-      />
-      <Toaster />
-    </div>
+        <Image
+          quality={100}
+          alt="dois celulares com o aplicativo do NLW aberto"
+          src={AppNlwMobileImg}
+        />
+        <Toaster />
+      </div>
+      <Analytics />
+    </>
   )
 }
